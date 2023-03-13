3927e7283cd289e99e3ec6dc35ea75da2ec0af72
gitanupmannem <anup.mannem@gmail.com>
2023-03-13 11:39:23 +0530

    prisma setup with example

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,55 @@
+import { prisma } from '$lib/server/prisma';
+import { fail } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async () => {
+  return {
+    articles: await prisma.article.findMany()
+  }
+};
+
+export const actions: Actions = {
+  createArticle: async ({ request }) => {
+    // get title and content from formdata
+    const { title, content } = Object.fromEntries(await request.formData()) as { title: string, content: string }
+
+    // create new article in db
+    try {
+      await prisma.article.create({
+        data: {
+          title,
+          content
+        }
+      })
+    } catch (err) {
+      console.error(err)
+      return fail(500, { message: 'Could not create article' })
+    }
+
+    return {
+      status: 201,
+
+    }
+  },
+
+  deleteArticle: async ({ url }) => {
+    const id = url.searchParams.get('id')
+    if (!id) {
+      return fail(400, { message: 'Invalid request' })
+    }
+
+    try {
+      await prisma.article.delete({
+        where: {
+          id: Number(id)
+        }
+      })
+    } catch (err) {
+      return fail(500, { message: 'Something went wrong deleting the article' })
+    }
+
+    return {
+      status: 200
+    }
+  }
+}
